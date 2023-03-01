d905e14537c502c02e5ff82be18dd1785b5f8edd
Chad <candycanekid77@gmail.com>
2023-03-01 16:14:48 -0700

    move user accts to db

--- pages/process.php
+++ pages/process.php
@@ -5,11 +5,16 @@
     $email = $_POST['email'];
     $pwd = password_hash($_POST['pwd'], PASSWORD_DEFAULT);
 
-    $CSVStr = "$firstName, $lastName, $email, $pwd".PHP_EOL;
+    $servername = "localhost";
+    $username = "steverq1_chad";
+    $password = "Csci213+#003";
+    $dbname = "steverq1_chad";
 
-    $usersFile = fopen("../data/users.txt", "a");
-    fwrite($usersFile, $CSVStr);
-    fclose($usersFile);
+    $db_conn = new mysqli($servername, $username, $password, $dbname);
+
+    $db_conn->query(
+        "INSERT INTO Users VALUES(NULL, '$firstName', '$lastName', '$email', '$pwd');"
+    );
 
     header("Location: ../index.php?ACRedirect=''");
 
