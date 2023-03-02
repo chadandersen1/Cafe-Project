6f18c440e2c7760463ed5e4d0e8cdb3e980aeca0
Chad <candycanekid77@gmail.com>
2023-03-01 18:09:56 -0700

    fix banner

--- includes/banner.php
+++ includes/banner.php
@@ -13,7 +13,7 @@ echo "  <header>
                 <p id='phone'>Phone: (406)761-3506</p>
                 <p>Hours:<br/>M-Th: 7:00 AM - 9:00 PM<br/>
                 F-Su: &nbsp;7:00 AM - 10:00PM </p>
-                <a href='".PAGE_PATH."create_acct.php'>Create Account</a><p> | </p><a href='".PAGE_PATH."login.php'>Login</a>
+                <p><a href='".PAGE_PATH."create_acct.php'>Create Account</a> | <a href='".PAGE_PATH."login.php'>Login</a></p>
             </div>
         </header>";
 ?>
